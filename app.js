0b772603e571359bbca34c2099b0864ca7e0cf63
nooko69 <dylan.danelpro@gmail.com>
2024-12-28 21:23:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const produits = [
-        { nom: 'Produit 1', prix: 20, description: 'Description du produit 1', lien: 'https://fr.aliexpress.com/item/1005002754562453.html' },
-        { nom: 'Produit 2', prix: 30, description: 'Description du produit 2', lien: 'https://fr.aliexpress.com/item/1005002754562453.html' },
-        { nom: 'Produit 3', prix: 40, description: 'Description du produit 3', lien: 'https://fr.aliexpress.com/item/1005002754562453.html' },
+        { nom: 'Lampe Champignon', prix: 20, description: 'Lampe décorative en forme de champignon', lien: 'https://fr.aliexpress.com/item/1005002754562453.html' },
+        { nom: 'Litière de Tofu pour Chats', prix: 15, description: 'Litière écologique pour chats', lien: 'https://fr.aliexpress.com/item/1005002754562453.html' },
+        { nom: 'Bandes de Résistance Personnalisées', prix: 25, description: 'Bandes de résistance pour le fitness', lien: 'https://fr.aliexpress.com/item/1005002754562453.html' },
+        { nom: 'Diffuseur d\'Huiles Essentielles', prix: 35, description: 'Diffuseur d\'huiles essentielles pour la relaxation', lien: 'https://fr.aliexpress.com/item/1005002754562453.html' },
+        { nom: 'Chargeur Sans Fil', prix: 30, description: 'Chargeur sans fil innovant', lien: 'https://fr.aliexpress.com/item/1005002754562453.html' },
+        { nom: 'Stylos Recyclés', prix: 10, description: 'Stylos fabriqués à partir de matériaux recyclés', lien: 'https://fr.aliexpress.com/item/1005002754562453.html' },
+        { nom: 'Interrupteur Connecté', prix: 40, description: 'Interrupteur intelligent pour la maison connectée', lien: 'https://fr.aliexpress.com/item/1005002754562453.html' },
+        { nom: 'Sac de Voyage Antitache', prix: 50, description: 'Sac de voyage innovant et antitache', lien: 'https://fr.aliexpress.com/item/1005002754562453.html' },
+        { nom: 'Bijoux Personnalisés', prix: 45, description: 'Bijoux personnalisés avec gravure', lien: 'https://fr.aliexpress.com/item/1005002754562453.html' },
+        { nom: 'Ustensiles de Cuisine Multifonctions', prix: 25, description: 'Ustensiles de cuisine innovants', lien: 'https://fr.aliexpress.com/item/1005002754562453.html' }
     ];
 
     const produitsSection = document.getElementById('produits');
